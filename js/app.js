cbd4a7103924215853c404d912652facd8c663c1
Michael Reed <michael.reed315@gmail.com>
2016-06-01 18:30:36 -0400

    Added foundation CSS to buttons to make them stand out

--- js/app.js
+++ js/app.js
@@ -18,12 +18,15 @@ var app = {
   buildNameItem: function(name) {
     var li = document.createElement('li');
     li.innerText = name;
+    li.style.border = '1px solid black';
     var deleteButton = document.createElement('button');
     deleteButton.onclick = this.deleteName;
     deleteButton.innerText = 'Delete';
+    deleteButton.className = 'button alert';
     var promote = document.createElement('button');
     promote.onclick = this.promoteName;
     promote.innerText = 'Promote';
+    promote.className = 'button';
     li.appendChild(deleteButton);
     li.appendChild(promote);
     return li;
@@ -34,6 +37,7 @@ var app = {
     var roster = document.querySelector('.roster');
     var firstName = this.firstName.value;
     roster.insertBefore(app.buildNameItem(firstName), roster.childNodes[0]);
+    this.reset();
   }
 };
 
