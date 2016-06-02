64c17e4c733dac409523182fa0d427cd87eebad1
Michael Reed <michael.reed315@gmail.com>
2016-06-02 15:38:57 -0400

    Removed commented code from previous verison. This is a refactored prettier version than the homewrok one

--- js/app.js
+++ js/app.js
@@ -17,7 +17,7 @@ var megaRoster = {
     var studentName = form.studentName.value;
     var listItem = this.buildListItem(studentName);
     var studentList = document.querySelector('#studentList')
-    studentList.insertBefore(listItem, studentList.childNodes[0]);
+    studentList.insertBefore(listItem, studentList.firstChild);
     this.count += 1;
     form.reset();
     form.studentName.focus();
@@ -50,44 +50,6 @@ var megaRoster = {
     link.onclick = options.handler;
     return link;
   }
-
-  // init: function() {
-  //   var myForm = document.querySelector('form');
-  //   myForm.onsubmit = this.addNameToDetails;
-  // },
-  //
-  // deleteName: function(ev) {
-  //   this.parentNode.remove();
-  // },
-  //
-  // promoteName: function(ev) {
-  //   this.parentNode.style.border = '1px solid red';
-  // },
-  //
-  // buildNameItem: function(name) {
-  //   var li = document.createElement('li');
-  //   li.innerText = name;
-  //   li.style.border = '1px solid black';
-  //   var deleteButton = document.createElement('button');
-  //   deleteButton.onclick = this.deleteName;
-  //   deleteButton.innerText = 'Delete';
-  //   deleteButton.className = 'button alert';
-  //   var promote = document.createElement('button');
-  //   promote.onclick = this.promoteName;
-  //   promote.innerText = 'Promote';
-  //   promote.className = 'button';
-  //   li.appendChild(deleteButton);
-  //   li.appendChild(promote);
-  //   return li;
-  // },
-  //
-  // addNameToDetails: function(ev) {
-  //   ev.preventDefault();
-  //   var roster = document.querySelector('.roster');
-  //   var firstName = this.firstName.value;
-  //   roster.insertBefore(app.buildNameItem(firstName), roster.childNodes[0]);
-  //   this.reset();
-  // }
 };
 
 megaRoster.init();
