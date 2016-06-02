648bb2df4243a81ebc5fa1e7335c6a7ad1355378
Michael Reed <michael.reed315@gmail.com>
2016-06-02 14:03:57 -0400

    Refactoring. Added columns in form

--- js/app.js
+++ js/app.js
@@ -1,44 +1,55 @@
 $(document).foundation();
 
-var app = {
+var megaRoster = {
 
   init: function() {
-    var myForm = document.querySelector('form');
-    myForm.onsubmit = this.addNameToDetails;
+      document.querySelector('form').onsubmit = this.addStudent;
   },
 
-  deleteName: function(ev) {
-    this.parentNode.remove();
-  },
-
-  promoteName: function(ev) {
-    this.parentNode.style.border = '1px solid red';
-  },
-
-  buildNameItem: function(name) {
-    var li = document.createElement('li');
-    li.innerText = name;
-    li.style.border = '1px solid black';
-    var deleteButton = document.createElement('button');
-    deleteButton.onclick = this.deleteName;
-    deleteButton.innerText = 'Delete';
-    deleteButton.className = 'button alert';
-    var promote = document.createElement('button');
-    promote.onclick = this.promoteName;
-    promote.innerText = 'Promote';
-    promote.className = 'button';
-    li.appendChild(deleteButton);
-    li.appendChild(promote);
-    return li;
-  },
-
-  addNameToDetails: function(ev) {
+  addStudent: function(ev) {
     ev.preventDefault();
-    var roster = document.querySelector('.roster');
-    var firstName = this.firstName.value;
-    roster.insertBefore(app.buildNameItem(firstName), roster.childNodes[0]);
-    this.reset();
+    alert('submitted');
   }
+
+
+
+  // init: function() {
+  //   var myForm = document.querySelector('form');
+  //   myForm.onsubmit = this.addNameToDetails;
+  // },
+  //
+  // deleteName: function(ev) {
+  //   this.parentNode.remove();
+  // },
+  //
+  // promoteName: function(ev) {
+  //   this.parentNode.style.border = '1px solid red';
+  // },
+  //
+  // buildNameItem: function(name) {
+  //   var li = document.createElement('li');
+  //   li.innerText = name;
+  //   li.style.border = '1px solid black';
+  //   var deleteButton = document.createElement('button');
+  //   deleteButton.onclick = this.deleteName;
+  //   deleteButton.innerText = 'Delete';
+  //   deleteButton.className = 'button alert';
+  //   var promote = document.createElement('button');
+  //   promote.onclick = this.promoteName;
+  //   promote.innerText = 'Promote';
+  //   promote.className = 'button';
+  //   li.appendChild(deleteButton);
+  //   li.appendChild(promote);
+  //   return li;
+  // },
+  //
+  // addNameToDetails: function(ev) {
+  //   ev.preventDefault();
+  //   var roster = document.querySelector('.roster');
+  //   var firstName = this.firstName.value;
+  //   roster.insertBefore(app.buildNameItem(firstName), roster.childNodes[0]);
+  //   this.reset();
+  // }
 };
 
-app.init();
+megaRoster.init();
