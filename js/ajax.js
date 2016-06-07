16e32acb661f030359d0d6471852b367b995a641
Michael Reed <michael.reed315@gmail.com>
2016-06-07 09:52:47 -0400

    Pull class version of megaRoster

--- js/ajax.js
+++ js/ajax.js
@@ -1,6 +1,22 @@
 $.extend(megaRoster, {
-  setupAllMutantsAjax: function() {
+  setupAjax: function() {
     $('a[data-remote="true"]').on('click', function(ev) {
+      ev.preventDefault();
+      $.ajax({
+        url: $(ev.currentTarget).attr('href'),
+        method: 'get',
+        dataType: 'jsonp',
+        jsonpCallback: "callback",
+        context: this,
+        success: function(data) {
+          this.loadResults(data);
+        }
+      });
+    }.bind(megaRoster));
+  },
+
+  setupMutantAjax: function() {
+    $('a[data-remote-mutants="true"]').on('click', function(ev) {
       ev.preventDefault();
       $.ajax({
         url: $(ev.currentTarget).attr('href'),
@@ -9,23 +25,23 @@ $.extend(megaRoster, {
           this.loadMutants(data);
         }.bind(this)
       })
-    }.bind (megaRoster));
+    }.bind(megaRoster));
   },
 
   loadMutants: function(data) {
     $.each(data, function(i, mutant) {
-      megaRoster.prependChild(megaRoster.studentList, megaRoster.buildListItem({
-        studentName: '<i class="fa fa-android"></i>' + mutant.mutant_name + '[' + mutant.real_name + '](' + mutant.power + ')',
-        favorited: false
-      }));
+      this.addStudent({
+        name: mutant.mutant_name,
+        id: mutant.id
+      });
     }.bind(this));
   },
 
-  setupClearAjax: function() {
-    $('#clear').on('click', function(ev) {
-      ev.preventDefault();
-      localStorage.clear();
-      this.studentList.empty();
-    }.bind(this));
-  }
+  loadResults: function(data) {
+    if (data.students) {
+      $.each(data.students, function(index, student) {
+        this.addStudent(student, true);
+      }.bind(this));
+    }
+  },
 });
